d6c3ebac4549b5b6f11c41c3a38162c81a9fc714
hristo-grudev <hr.grudev@gmail.com>
2021-03-15 11:06:06 +0200

    Job is done

---
+++ README.md
@@ -0,0 +1,8 @@
+URL: https://www.bnpparibas.hu/en/news-press/all-our-news/
+
+Spider name: bnpparibashu
+DB Schema:
+
+title
+content
+date
---
+++ bnpparibashu/spiders/spider.py
@@ -0,0 +1,33 @@
+import scrapy
+
+from scrapy.loader import ItemLoader
+
+from ..items import BnpparibashuItem
+from itemloaders.processors import TakeFirst
+
+
+class BnpparibashuSpider(scrapy.Spider):
+	name = 'bnpparibashu'
+	start_urls = ['https://www.bnpparibas.hu/en/news-press/all-our-news/']
+
+	def parse(self, response):
+		post_links = response.xpath('//h2/a/@href')
+		yield from response.follow_all(post_links, self.parse_post)
+
+		next_page = response.xpath('//a[@class="nextpostslink"]/@href')
+		yield from response.follow_all(next_page, self.parse)
+
+	def parse_post(self, response):
+		title = response.xpath('//h1/text()').get()
+		description = response.xpath('//div[@class="entry-content"]/descendant-or-self::*/text()').getall()
+		description = [p.strip() for p in description]
+		description = ' '.join(description)
+		date = response.xpath('//span[@class="date"]/text()').get()
+
+		item = ItemLoader(item=BnpparibashuItem(), response=response)
+		item.default_output_processor = TakeFirst()
+		item.add_value('title', title)
+		item.add_value('description', description)
+		item.add_value('date', date)
+
+		return item.load_item()
